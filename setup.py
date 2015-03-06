1999e658fa82265b9c722715fd18aa6e92828188
Erik de Jonge <erik@a8.nl>
2015-03-06 10:54:05 +0100

    -

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='consoleprinter',
-      version='4',
+      version='5',
       description='Console printer with linenumbers, stacktraces, logging, conversions and coloring..',
       url='https://github.com/erikdejonge/consoleprinter',
       author='Erik de Jonge',
