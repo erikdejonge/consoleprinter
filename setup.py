a4004dbdfbbb87a7406d8facc57dd6342e9e221f
Erik de Jonge <erik@a8.nl>
2015-03-06 12:34:54 +0100

    pip Friday 06 March 2015 (week:9 day:65), 12:34:51

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='consoleprinter',
-      version='8',
+      version='9',
       description='Console printer with linenumbers, stacktraces, logging, conversions and coloring..',
       url='https://github.com/erikdejonge/consoleprinter',
       author='Erik de Jonge',
