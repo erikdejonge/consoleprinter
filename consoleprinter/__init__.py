f30611a217982d12a4cc8a461aa0ccc521dda5a7
Erik de Jonge <erik@a8.nl>
2015-05-05 11:03:12 +0200

    Styles Tuesday 05 May 2015 (week:18 day:125), 11:03:09

--- consoleprinter/__init__.py
+++ consoleprinter/__init__.py
@@ -6,9 +6,9 @@ console
 Active8 (05-03-15)
 license: GNU-GPL2
 """
+
 from __future__ import division, print_function, absolute_import, unicode_literals
 from future import standard_library
-
 import io
 import os
 import re
@@ -26,7 +26,6 @@ import collections
 import unicodedata
 
 from sh import clear, whoami
-
 SINGULARS = [
     (r"(?i)(database)s$", r'\1'),
     (r"(?i)(quiz)zes$", r'\1'),
@@ -1380,6 +1379,50 @@ def exist(data):
     return True
 
 
+def forceascii(unicrap):
+    """
+    @type unicrap: str
+    @return: None
+    """
+    xlate = {0xc0: 'A', 0xc1: 'A', 0xc2: 'A', 0xc3: 'A', 0xc4: 'A', 0xc5: 'A',
+             0xc6: 'Ae', 0xc7: 'C',
+             0xc8: 'E', 0xc9: 'E', 0xca: 'E', 0xcb: 'E',
+             0xcc: 'ival', 0xcd: 'ival', 0xce: 'ival', 0xcf: 'ival',
+             0xd0: 'Th', 0xd1: 'N',
+             0xd2: 'O', 0xd3: 'O', 0xd4: 'O', 0xd5: 'O', 0xd6: 'O', 0xd8: 'O',
+             0xd9: 'U', 0xda: 'U', 0xdb: 'U', 0xdc: 'U',
+             0xdd: 'Y', 0xde: 'th', 0xdf: 'ss',
+             0xe0: 'a', 0xe1: 'a', 0xe2: 'a', 0xe3: 'a', 0xe4: 'a', 0xe5: 'a',
+             0xe6: 'ae', 0xe7: 'c',
+             0xe8: 'e', 0xe9: 'e', 0xea: 'e', 0xeb: 'e',
+             0xec: 'ival', 0xed: 'ival', 0xee: 'ival', 0xef: 'ival',
+             0xf0: 'th', 0xf1: 'n',
+             0xf2: 'o', 0xf3: 'o', 0xf4: 'o', 0xf5: 'o', 0xf6: 'o', 0xf8: 'o',
+             0xf9: 'u', 0xfa: 'u', 0xfb: 'u', 0xfc: 'u',
+             0xfd: 'y', 0xfe: 'th', 0xff: 'y',
+             0xa1: '!', 0xa2: '{cent}', 0xa3: '{pound}', 0xa4: '{currency}',
+             0xa5: '{yen}', 0xa6: '|', 0xa7: '{section}', 0xa8: '{umlaut}',
+             0xa9: '{C}', 0xaa: '{^a}', 0xab: '<<', 0xac: '{not}',
+             0xad: '-', 0xae: '{R}', 0xaf: '_', 0xb0: '{degrees}',
+             0xb1: '{+/-}', 0xb2: '{^2}', 0xb3: '{^3}', 0xb4: "'",
+             0xb5: '{micro}', 0xb6: '{paragraph}', 0xb7: '*', 0xb8: '{cedilla}',
+             0xb9: '{^1}', 0xba: '{^o}', 0xbb: '>>',
+             0xbc: '{1/4}', 0xbd: '{1/2}', 0xbe: '{3/4}', 0xbf: '?',
+             0xd7: '*', 0xf7: '/'}
+
+    result = ''
+
+    for ival in unicrap:
+        if ord(ival) in xlate:
+            result += xlate[ord(ival)]
+        elif ord(ival) >= 0x80:
+            pass
+        else:
+            result += str(ival)
+
+    return result
+
+
 def format_source_code_line_console(path):
     """
     @type path: str, unicode
@@ -1954,13 +1997,6 @@ def log_date_time_string():
     return ts
 
 
-def main():
-    """
-    main
-    """
-    console("Consoleprinter is a library for use in commandline application")
-
-
 def mill(it, label='', hide=None, expected_size=None, every=1):
     """
     @type it: iterator
@@ -2198,14 +2234,6 @@ def remove_extra_indentation(doc, stop_looking_when_encountered=None):
 
     newdoc = newdoc.strip()
     return newdoc
-
-
-def stripall(astr):
-    """
-    @type astr: str
-    @return: None
-    """
-    return remove_extra_indentation(astr)
 
 
 def reset_terminal():
@@ -2588,6 +2616,14 @@ def strcmp(s1, s2):
     return equal
 
 
+def stripall(astr):
+    """
+    @type astr: str
+    @return: None
+    """
+    return remove_extra_indentation(astr)
+
+
 def stty_sane():
     """
     stty_sane
@@ -2662,8 +2698,14 @@ def warning(command, description):
     console_cmd_desc(command, description, "red", enteraftercmd=False)
 
 
-SystemGlobals()
+def main():
+    """
+    main
+    """
+    console("Consoleprinter is a library for use in commandline application")
 
+
+SystemGlobals()
 _irregular('child', 'children')
 _irregular('cow', 'kine')
 _irregular('man', 'men')
@@ -2671,11 +2713,8 @@ _irregular('move', 'moves')
 _irregular('person', 'people')
 _irregular('sex', 'sexes')
 _irregular('zombie', 'zombies')
-
 set_console_start_time()
-
 standard_library.install_aliases()
-
 
 if __name__ == "__main__":
     main()
