a3136b0235c8f8e39870165577a5fdb5af949ca3
Erik de Jonge <erik@a8.nl>
2015-06-24 16:43:22 +0200

    James Monroe: Isnt history ultimately the result of our fear of boredom? Wednesday 24 June 2015 (week:25 day:175), 16:43:20

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='consoleprinter',
-      version='58',
+      version='59',
       description='Console printer with linenumbers, stacktraces, logging, conversions and coloring..',
       url='https://github.com/erikdejonge/consoleprinter',
       author='Erik de Jonge',
